862ab6d1d9f46a396174e31117c4474241fbcc87
Michael Pedersen <steele.pedersen@gmail.com>
2016-09-25 21:33:28 -0400

    #17 Can now query Twitter API
    The index page now queries Twitter using the value of the "topic" url parameter. Only results with links should be displayed. Try it with "http://localhost:3000/?topic=sports". You'll have to supply the Twitter API key which I'll link in the team chat.

--- index.js
+++ index.js
@@ -20,11 +20,46 @@ app.use('/static', express.static(__dirname + '/static'));
 //Redirect favicon to static directory
 app.use(favicon(__dirname + '/static/favicon.ico'));
 
-//The main app
-app.use('/', function (req, res) {
-	res.render('pages/index');
+//Main website
+//Typical Example:    http://localhost:3000/
+//Example with query: http://localhost:3000/?topic=sports
+app.use('/', function(req, res) {
+    var topicQuery = req.query.topic || "";   //if undefined set to ""
+
+    if (topicQuery == ""){            //No '?topic=whatever' in url
+      res.render('pages/index');      //Return default index page
+      return;                         //Stop executing this request
+    }
+
+    //Setup Twitter Parameters
+    var params = {
+        q: topicQuery + " filter:links",  //results must have links
+        result_type: 'popular',      //sort by popularity
+        count: '100',                //Return 100 results (Twitter's max is 100)
+    }
+
+    //DEV: Logging the parameters used
+    console.log("Twitter Parameters: ", params);
+
+    //TODO: Check rate limiting
+
+    //Send GET request
+    client.get('search/tweets', params, function(error, tweets, response) {
+        // console.log(tweets); //DEV: Used to see the raw json returned by Twitter
+        if (!error) {   //All is well
+            res.render('pages/index', {          //Render the index page
+                topic: topicQuery,               //our search query ex. 'sports'
+                tweetResults: tweets,            //the json of the tweets
+                count: tweets.statuses.length,   //how many tweet results
+            });
+        } else {
+            console.log(error);       //Something went wrong, print to console
+        }
+    });
 });
 
 app.listen(3000, function() {
-	console.log('listening on port 3000');
+    console.log('Listening on port 3000.');
+    console.log('URL: http://localhost:3000/');
+    console.log('JSON URL: http://localhost:3000/?topic=sports');
 });
