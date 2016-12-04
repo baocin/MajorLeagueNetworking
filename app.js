a486d741fe82bd263318c5f2883d08cde843724c
Michael Pedersen <steele.pedersen@gmail.com>
2016-12-03 19:45:40 -0500

    Cleaning up after merge
    Forgot to change the static resources route in app.js so all css/js/images weren't being displayed

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'public')));
 
 //Routing for static assets
-app.use('/static', express.static(__dirname + '/static'));
+app.use('/', express.static(__dirname + '/static'));
 
 //routes
 app.use('/', routes);
